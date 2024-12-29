ac8785672200a15e4c2dc88d6724797f0329296b
Alena Radomskaia <radomskaia.a.i@gmail.com>
2024-12-30 01:35:12 +0700

    feat: add think the word

--- hangman/src/js/app.js
+++ hangman/src/js/app.js
@@ -1,7 +1,13 @@
 import { createDOMTree } from './createDOMTree.js';
 import { keyboard } from './keyboard.js';
+import { thinkWord } from './thinkWord.js';
+import * as constants from 'node:constants';
 
 export function app() {
   const elements = createDOMTree();
-  keyboard(elements.keyboardWrapper);
+  const [word, wordLetters] = thinkWord({
+    wordWrapper: elements.wordWrapper,
+    hint: elements.hint,
+  });
+  keyboard(elements.keyboardWrapper, word, wordLetters, elements.image);
 }
