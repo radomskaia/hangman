260002ba5c0b6a554c4bc65ce2b490c1135e0073
Alena Radomskaia <radomskaia.a.i@gmail.com>
2025-01-08 12:51:17 +0700

    docs: change JSDocs

--- hangman/src/js/resetGame.js
+++ hangman/src/js/resetGame.js
@@ -2,11 +2,7 @@ import { gameState, startGame } from './startGame.js';
 
 /**
  * Resets the game state, updates the UI elements, and starts a new game.
- * @param {Object} elements - An object containing the DOM elements used in the game.
- * @param {HTMLImageElement} elements.image - The image element representing the gallows.
- * @param {HTMLElement} elements.guessesCounter - The element displaying the number of guesses.
- * @param {HTMLElement} elements.wordWrapper - The container for the guessed letters of the word.
- * @param {Object} elements.keyboardKeys - An object containing the keyboard button elements.
+ * @param { Object } elements - An object containing the DOM elements used in the game.
  */
 export function resetGame(elements) {
   elements.image.src = './images/empty-gallows.png';
